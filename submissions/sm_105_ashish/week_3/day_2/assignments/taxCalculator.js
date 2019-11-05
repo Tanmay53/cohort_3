70cfe241bd0892e07d6c9396e95394c1453d29f6
Ashish379k <au.ashish1@gmail.com>
2019-11-05 22:46:09 +0530

    W3 D2 A

--- submissions/sm_105_ashish/week_3/day_2/assignments/taxCalculator.js
+++ submissions/sm_105_ashish/week_3/day_2/assignments/taxCalculator.js
@@ -1,51 +1,56 @@
-var TotalAmount=2000000;
-var SavingsAmount=500000;
-
+var TotalAmount=250100;
+var SavingsAmount=100000;
 if(TotalAmount>1000000)
-{
-    var TaxableAmount=TotalAmount-0.1*SavingsAmount;
-    var Rebate=0.1*SavingsAmount*0.3;
-    if (Rebate>50000)
-    {    var AdditionalTax=Rebate-50000;
-        if(AdditionalTax>0)
-            {
-               var RebateAmount=50000;
-            }
-        else
-            {
-               var RebateAmount=Rebate;
-            }
-    }
-    else
-    {
-        var AdditionalTax=0;
-        var RebateAmount=Rebate;
-    }
-    var TaxAmount=(TaxableAmount-1000000)*0.3+125000+AdditionalTax;
+{    
+    var Deduction=0.1*SavingsAmount;
+      if(Deduction>=50000)
+      {
+         Deduction=50000;
+      }
+    var TaxableAmount=TotalAmount-Deduction;
+    
+      if (TaxableAmount>1000000)
+        {    
+        var TaxAmount=(TaxableAmount-1000000)*0.3+125000
+        }
+      else
+        {
+        TaxAmount=(TaxableAmount-500000)*0.2+25000;
+        }
     var NetIncome=TotalAmount-TaxAmount;
     console.log('your Total tax deduction is:'+TaxAmount)
     console.log('your Total Income after tax deduction is:'+NetIncome)
-    console.log('your Total rebate in tax deduction by saving amount is:'+RebateAmount)
 }
 else if(TotalAmount>500000)
 {
         var TaxableAmount=TotalAmount-0.3*SavingsAmount;
-        var Rebate=0.2*SavingsAmount*0.3;
-        var TaxAmount=(TaxableAmount-500000)*0.2+25000;
+        if(TaxableAmount>500000)
+        {
+            var TaxAmount=(TaxableAmount-500000)*0.2+25000;
+        }
+        else
+        {
+            TaxAmount=(TaxableAmount-250000)*0.1
+        }
         var NetIncome=TotalAmount-TaxAmount;
         console.log('your Total tax deduction is:'+TaxAmount)
         console.log('your Total Income after tax deduction is:'+NetIncome)
-        console.log('your Total rebate in tax deduction by saving amount is:'+Rebate)
+        
 }
 else if(TotalAmount>250000)
 {
     var TaxableAmount=TotalAmount-0.5*SavingsAmount;
-    var Rebate=0.1*SavingsAmount*0.5;
-    var TaxAmount=(TaxableAmount-250000)*0.1+AdditionalTax;
+    if(TaxableAmount>250000)
+        {
+            var TaxAmount=(TaxableAmount-250000)*0.1;
+        }
+        else
+        {
+            TaxAmount=0;
+        }
     var NetIncome=TotalAmount-TaxAmount;
     console.log('your Total tax deduction is:'+TaxAmount)
     console.log('your Total Income after tax deduction is:'+NetIncome)
-    console.log('your Total rebate in tax deduction by saving amount is:'+Rebate)
 }
 else
 {
