06fe93c344e8115e034c1672ca6f00838ba602c5
Soumik Acharjee <acharjee266@gmail.com>
2019-11-05 17:58:07 +0530

    W3 D2 S2

---
+++ submissions/sm_112_soumik/week_03/day_2/session_2/ageCheck.js
@@ -0,0 +1,10 @@
+var age =18
+if(age > 18){
+console.log("You are eligible to drive")
+}
+else if(age < 18){
+    console.log("You Cant drive in India!")
+}
+else{
+    console.log("You're just old enough to drive in India")
+}
