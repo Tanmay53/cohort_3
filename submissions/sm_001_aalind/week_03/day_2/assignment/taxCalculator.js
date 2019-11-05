816c745e2a290aeca1d003f869a76b678a9df4ac
Aalind Sharma <aalinds.sharma@gmail.com>
2019-11-05 17:13:31 +0530

    W3 D2 A

--- submissions/sm_001_aalind/week_03/day_2/assignment/taxCalculator.js
+++ submissions/sm_001_aalind/week_03/day_2/assignment/taxCalculator.js
@@ -1,33 +1,30 @@
 var income;
 var savings;
 var income_tax;
-var rebate;
+var rebate = 0;
 
 if (income <= 500000) {
-	rebate = savings*.50;
-	if (rebate > 50000) {
-		rebate = 50000
-	}
+	rebate = savings * .50;
 } else if (income <= 1000000) {
-	rebate = savings*.30;
-	if (rebate > 50000) {
-		rebate = 50000
-	}
+	rebate = savings * .30;
 } else if (income > 1000000 ) {
-	rebate = savings*.10;
-	if (rebate > 50000) {
-		rebate = 50000
-	}
+	rebate = savings * .10;
 }
 
-if(income-rebate < 250000) {
+if (rebate > 50000) {
+	rebate = 50000;
+}
+
+income = income - rebate;
+
+if(income < 250000) {
 	income_tax = 0;
-} else if (income-rebate >= 250000 && income-rebate <= 500000) {
+} else if (income >= 250000 && income <= 500000) {
 	income_tax = (income - 250000) * .10;
-} else if (income-rebate > 500000 && income-rebate <= 1000000) {
-	income_tax = 250000*.10 + (income - 500000)*.20;
-} else if (income-rebate > 1000000) {
-	income_tax = 250000*.10 + 500000*.20 + (income-1000000)*.30;
+} else if (income > 500000 && income <= 1000000) {
+	income_tax = 250000 * .10 + (income - 500000) * .20;
+} else if (income > 1000000) {
+	income_tax = 250000 * .10 + 500000 * .20 + (income - 1000000) * .30;
 }
 
 console.log(income_tax)
