4d6873946d5290a6d0ccd21f24ad45ec47eb7ca0
Hasmuddin <mdaliansari33@gmail.com>
2019-11-06 19:40:57 +0530

    W3 D3 S2

--- submissions/sm_107_hasmuddin/week_3/day_3/session_2/average_divisible_while.js
+++ submissions/sm_107_hasmuddin/week_3/day_3/session_2/average_divisible_while.js
@@ -1,10 +1,13 @@
 var numbers = [1, 2, 3, 4, 5];
 var len = numbers.length;
-sum = 0;
-var i = 0;
-while (i < len) {
-  sum += numbers[i];
-  i++;
+function averageNumber() {
+  sum = 0;
+  var i = 0;
+  while (i < len) {
+    sum += numbers[i];
+    i++;
+  }
+  average = sum / len;
+  console.log(average);
 }
-average = sum / len;
-console.log(average);
+averageNumber();
