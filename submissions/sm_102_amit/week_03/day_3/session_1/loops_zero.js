62acc05cf2dcbc2b572656bf425f755b55970d99
Amit Kumar <akamit21@outlook.com>
2019-11-06 13:00:14 +0530

    W3 D3 C1

---
+++ submissions/sm_102_amit/week_03/day_3/session_1/loops_zero.js
@@ -0,0 +1,21 @@
+// for loop
+function forLoop(input) {
+  if (Number(input) > 0) {
+    for (var i = input; i >= 0; i--) {
+      console.log(i);
+    }
+  } else {
+    console.log("Wrong Input");
+  }
+}
+forLoop(9);
+
+// input case
+// -4, 0, 20
+
+// while loop logic
+// var i = 25;
+// while (i >= 0) {
+//   console.log(i);
+//   i--;
+// }
