03b4aac9cbd3fce1ceb8c125f11a0d0d66c27fa2
krsnaapoorv <apoorva15997@gmail.com>
2019-11-06 09:43:31 +0530

    W3 D2 A

--- submissions/sm_103_apoorva/week_03/day_2/assignments/taxCalculator.js
+++ submissions/sm_103_apoorva/week_03/day_2/assignments/taxCalculator.js
@@ -24,7 +24,7 @@ else if(250000< income < 500000){
     }
 }
 else if(500000<= income <= 1000000){
-    if(saving>166666){
+    if(saving>166666){ //166666 = cap 50000/0.3.have fixed rebate
         rebate=50000;
         ti=income-rebate;
     }
@@ -37,11 +37,13 @@ else if(500000<= income <= 1000000){
     {
         s=(ti-1000000)*0.3;
         tax=s+125000;
+        //125000=0.2*500000+0.1*250000
     }
     else if(ti<1000000)
     {
         s=(ti-500000)*0.2;
         tax=s+25000;
+        //25000 as it 0.1% of 250000
     }
     else if(ti<500000)
     {
