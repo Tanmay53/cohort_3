86b6ba4724d3063ca5c715e0e8356ad73be2d9ee
Krishna Sharma <rohansharma.8574@gmail.com>
2019-11-08 12:50:46 +0530

    W3 D5 S1

--- submissions/sm_108_krishna-kant/week_03/day_5/session_1/removeDuplicatesArrays.js
+++ submissions/sm_108_krishna-kant/week_03/day_5/session_1/removeDuplicatesArrays.js
@@ -1,11 +1,21 @@
 var arr = [2, 3, 5, 7, 4, 8, 4, 7, 9];
 var filteredArray = [];
 
+// Index Of
+function indexOf(args, value) {
+  for (i = 0; i < args.length; i++) {
+    if (value == args[i]) {
+      return i;
+    }
+  }
+  return -1;
+}
+
 // function
 function removeDuplicates(args) {
   for (var i = 0; i < args.length; i++) {
     //   checking if element present in filtered array
-    if (filteredArray.indexOf(args[i]) == -1) {
+    if (indexOf(filteredArray, args[i]) == -1) {
       filteredArray.push(args[i]);
     }
   }
