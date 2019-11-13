320b4f38c13913e26693beea4b80268df3eadab5
Manish Sharma <manish1811.sharma@gmail.com>
2019-11-13 20:14:18 +0530

    W3 D5 C2

---
+++ submissions/sm_016_manish-sharma/week_3/day_5/session_2/newUser.js
@@ -0,0 +1,10 @@
+function newUser(inName,inEmail,inPassword){
+    var Object={
+        name: inName,
+        email: inEmail,
+        password: inPassword
+    }
+    console.log(Object)
+}
+
+newUser('Manish Sharma','manish@masai.com','sm_016')
