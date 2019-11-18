434f463a06f7de3d4c35f0cc134e436afc122e89
asheesh900 <thebusinessman999@gmail.com>
2019-11-18 18:30:05 +0530

    sprint_1

---
+++ submissions/sm_104_asheeh/week_05/day_1/sprint_1/scipt.js
@@ -0,0 +1,13 @@
+function register(){
+    var stud1 = document.getElementById("name").value;
+    var subject = document.getElementById("subject").value;
+    var marks = document.getElementById("marks").value;
+    var grade = document.getElementById("grade").value;
+
+    var info ={
+                 name : stud1,
+                 subject : subject ,
+                 marks : marks,
+                 grade : 
+    };
+}
