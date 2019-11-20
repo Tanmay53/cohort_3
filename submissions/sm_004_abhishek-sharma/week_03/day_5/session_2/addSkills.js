8f3394d7921f110a28e82f018b5617153f977246
Abhi1597 <abhishek.sharma1597@gmail.com>
2019-11-20 16:08:06 +0530

    W3 D5 S2

---
+++ submissions/sm_004_abhishek-sharma/week_03/day_5/session_2/addSkills.js
@@ -0,0 +1,20 @@
+var user = {
+    name : "Abhishek",
+    college : "BMSIT",
+    degree : "BTech",
+}
+console.log(user)
+console.log(propertyUpdate("skills", "Java Developer"))
+console.log(propertyUpdate( "skills", "Web Developer"))
+console.log(propertyUpdate( "skills", "Full Stack Developer"))
+
+function propertyUpdate(property, value){
+    for (key in user){
+        if(key == property){
+            user[key] = value
+        }
+        else
+            user[property] = value
+    }
+    return user
+}
