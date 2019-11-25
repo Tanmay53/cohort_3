6f75caad68ab172b90118d0948dfe22cf54793d4
Venkat Subbu <venkatasubbu913@gmail.com>
2019-11-25 19:18:28 +0530

    w3d3s2

--- submissions/sm_034_Venkat/week_3/day_3/session_2/count_consonants.js
+++ submissions/sm_034_Venkat/week_3/day_3/session_2/count_consonants.js
@@ -1,11 +1,14 @@
 function consonants(letter) {
-    var letter = letter || 'abcdEfg'
+    var letter = letter || 'abcdEfg';
+    count= 0;
     for (i = 0; i < letter.length; i++) {
         if (letter[i] != 'a' && letter[i] != 'e' && letter[i] != 'i' && letter[i] != 'o' && letter[i] != 'u' &&
             letter[i] != 'A' && letter[i] != 'E' && letter[i] != 'I' && letter[i] != 'O' && letter[i] != 'U') 
             {
-                console.log(letter[i])
+                console.log(letter[i]);
+                count++
             }
         }
+        console.log(count)
     }
     consonants('beNgalURu')
--- submissions/sm_034_Venkat/week_3/day_3/session_2/join_array.js
+++ submissions/sm_034_Venkat/week_3/day_3/session_2/join_array.js
@@ -1,8 +1,13 @@
 function show(chr, arr) {
-    var chr= chr || 'I';
-    var arr= arr || ['Am', 'Venkat', 'Subbu']
-    var array= chr.concat(","+ arr)
-    console.log(array)
-    console.log(typeof(array))
+    var chr = chr || 'a';
+    var arr = arr || ['Fruit', 'Vegetable', 'Stick'];
+    var joint= "";
+    var string= ""
+    for (i=0; i<arr.length; i++){
+      string=  chr.concat(""+arr[i]);
+        joint+=string;
+    }
+    console.log(joint)
+    console.log(typeof (joint))
 }
 show()
