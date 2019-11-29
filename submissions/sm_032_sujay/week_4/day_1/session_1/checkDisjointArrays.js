5a9679b593925dfd95dc42c62a47d48893ac1e28
Sujay N <sujayn18@gmail.com>
2019-11-29 22:27:22 +0530

    W4 D1 C1

---
+++ submissions/sm_032_sujay/week_4/day_1/session_1/checkDisjointArrays.js
@@ -0,0 +1,26 @@
+arr1 = [1,2,3,4,5]
+arr2= [6,7,8, null]
+
+function disjoint(a,b)
+{
+    check={}
+
+    for(i=0;i<a.length;i++)
+    {
+        check[a[i]]=a[i]
+        
+    }
+    // console.log(check)
+    for(j=0;j<b.length;j++)
+    {
+        check[b[j]]=b[j]
+    }
+    if( (Object.keys(check).length) == (a.length+b.length))
+    {
+        console.log('It is a disjoint Array')
+    }
+    else{
+        console.log("It aint disjoint yo!")
+    }
+
+}disjoint(arr1,arr2)
