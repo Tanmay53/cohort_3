2e97e32f12b50c713b1aa6a3fc3f7d1532dd1929
Sagar Kadu <sagarkadu17@gmail.com>
2019-12-03 23:54:53 +0530

    W07 D2 C1

--- submissions/sm_028_sagar/week_07/day_2/index.js
+++ submissions/sm_028_sagar/week_07/day_2/index.js
@@ -21,32 +21,31 @@ $('#submit').click(function() {
 
 $('#selectUser').click(function() {
     userData();
-})
+});
 
 //function to display user details 
 function userData() {
-
-    var user = document.getElementById('user');
-    var cont = document.createElement('div');
-    cont.setAttribute('class', 'container');
-    cont.setAttribute('id', 'selectUser');
-    var drop = document.createElement('select');
     for (var i = 0; i < userArr.length; i++) {
         var opt = document.createElement('option');
+        opt.setAttribute('value', i)
         opt.textContent = userArr[i].username;
-        opt.addEventListener('click', function() {
-            displayUser(userArr[i])
-        })
-        drop.append(opt)
+        $("#user").append(opt);
     }
-    cont.append(drop);
-    var btn = document.createElement('button');
-    btn.textContent = 'register';
-    user.append(cont)
-    user.append(btn)
 }
 
-//function for displaying user data for selected user form drop down
-function displayUser() {
 
+$('#user').change(function() {
+    let index = $('#user').val();
+    console.log(index)
+    displayUser(userArr[index])
+})
+
+//function for displaying user data for selected user form drop down
+function displayUser(user) {
+    console.log(user)
+    var doc = document.getElementById('userInfo')
+    doc.innerHTML = `<div>
+                           <div> Hello ${user.username}, welcome back </div>
+                           <div> Registered email is ${user.email}</div>
+                    </div>`
 }
