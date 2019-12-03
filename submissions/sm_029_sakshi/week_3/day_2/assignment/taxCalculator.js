4d2a2e83c5ce7ce86f1dbf68c67f43ef79c3fb5a
SakshiSinha <sakshisinha202326@gmail.com>
2019-12-03 18:27:08 +0530

    W3 D2 A

---
+++ submissions/sm_029_sakshi/week_3/day_2/assignment/taxCalculator.js
@@ -0,0 +1,34 @@
+var income = 200000 , rebate
+var savings = 500000
+tax = 0
+
+
+
+if (income >= 250000 && income < 500000){
+    rebate = savings * 0.50
+    rebate = (rebate > 50000) ? 50000 : rebate
+
+} else if(income >= 500000 && income < 1000000){
+    rebate = savings * 0.30
+    rebate = (rebate > 50000) ? 50000 : rebate
+
+} else if(income >= 1000000){
+    rebate = savings * 0.10
+    rebate = (rebate > 50000) ? 50000 : rebate
+}
+else{
+    console.log("no tax")
+}
+
+taxable_income = income - rebate
+
+if (taxable_income >= 250000 && taxable_income < 500000){
+    tax = taxable_income * 0.10
+
+} else if(taxable_income >= 500000 && taxable_income < 1000000){
+    tax = taxable_income * 0.20
+} else if(taxable_income >= 1000000){
+    tax = taxable_income * 0.30
+}
+
+console.log("Total payable Tax : ", tax)
