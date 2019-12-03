66e1e99421cf45d27fab00f3388ef2b0d6256b36
SakshiSinha <sakshisinha202326@gmail.com>
2019-12-03 20:38:45 +0530

    W3 D6 A

---
+++ submissions/sm_029_sakshi/week_3/day_6/assignment/reverseInteger.js
@@ -0,0 +1,13 @@
+function reverseInteger(num) {
+    var rem, quo = Number(num);
+    var rev_num = "";
+  
+    while(quo > 0) {
+      rem = quo % 10;
+      rev_num += rem;
+      quo = Math.floor(quo/10);
+    }
+  
+    return Number(rev_num);
+}
+console.log(reverseInteger(123456789))
