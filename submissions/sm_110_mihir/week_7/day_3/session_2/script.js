8140a08b8c79a5252057e94d7f581a101f097a2b
mihirlaldas <mihirlaldas@gmail.com>
2019-12-05 07:16:41 +0530

    W7D3S2

--- submissions/sm_110_mihir/week_7/day_3/session_2/script.js
+++ submissions/sm_110_mihir/week_7/day_3/session_2/script.js
@@ -10,22 +10,26 @@ $(document).ready(function() {
     });
 });
 function printData(students, resultsPerPage = 10) {
-    var pageno = 1,
-        index = 0;
+    var pageno = 0,
+        index;
     $("#pagenav").html("");
-    while (pageno * resultsPerPage <= students.length) {
-        console.log(pageno);
+    while (pageno * resultsPerPage < students.length) {
         $("#pagenav").append(
-            `<li class="page-item" id="page-${pageno}"><a class="page-link" href="#">${pageno}</a></li>`
+            `<li class="page-item" id="page-${pageno}"><a class="page-link" href="#">${pageno +
+                1}</a></li>`
         );
 
         $(`#page-${pageno}`).click(function() {
+            var currPage = $(this).text();
+            index = (currPage - 1) * resultsPerPage;
+            console.log(index);
             $("tbody").html("");
             for (let i = 1; i <= resultsPerPage; i++) {
                 let sum =
                     students[index].science +
                     students[index].maths +
                     students[index].english;
+                students[index].total = sum;
                 $("tbody").append(`<tr>
                                     <td>${students[index].id}</td>
                                     <td>${students[index].first_name}</td>
@@ -35,7 +39,7 @@ function printData(students, resultsPerPage = 10) {
                                     <td>${students[index].english}</td>
                                     <td>${students[index].maths}</td>
                                     <td>${students[index].science}</td>
-                                    <td>${sum}</td>
+                                    <td>${students[index].total}</td>
                             </tr>`);
                 index++;
             }
@@ -45,7 +49,48 @@ function printData(students, resultsPerPage = 10) {
 }
 
 // choosing results per page
-$("#resultsPerPage").click(() => {
+$("#resultsPerPage").click(() => trigger());
+function trigger() {
     var resultsPerPage = $("#resultsPerPage").val();
     printData(students, resultsPerPage);
+}
+// on click sort
+
+$(".btn").click(function() {
+    var sortID = $(this).attr("id");
+    var parameter = sortID.split("-");
+    console.log(parameter);
+    sortStudets(parameter[1], parameter[2]);
+});
+function sortStudets(sortBy, order) {
+    students.sort(compare);
+    function compare(a, b) {
+        const userA = a[sortBy];
+        const userB = b[sortBy];
+        let comparison = 0;
+        if (order == "asc") {
+            if (userA > userB) comparison = 1;
+            else if (userA < userB) comparison = -1;
+        } else if (order == "dsc") {
+            if (userA > userB) comparison = -1;
+            else if (userA < userB) comparison = 1;
+        }
+        return comparison;
+    }
+    console.log(students);
+    // trigger
+    $("#page-0").click();
+}
+
+// filter by gender
+$("#gender").click(function() {
+    var filteredStudents;
+    var gender = $(this).val();
+    if (gender == "all") filteredStudents = students;
+    else {
+        filteredStudents = students.filter(ele => ele.gender == gender);
+    }
+    console.log(filteredStudents);
+    printData(filteredStudents);
+    $("#page-0").click();
 });
