f8202e7d654c02edccb50d9017a3a3a68eb0b127
mihirlaldas <mihirlaldas@gmail.com>
2019-12-10 07:45:15 +0530

    W8D1S2

---
+++ submissions/sm_110_mihir/week_8/day_1/session_2/rjs1.5/src/components/contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+function Contact() {
+    return (
+        <div>
+            <button className="btn btn-danger btn-block mt-2">
+                CONTACT US{" "}
+            </button>
+        </div>
+    );
+}
+
+export default Contact;
