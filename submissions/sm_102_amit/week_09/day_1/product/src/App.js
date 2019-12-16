a4f65affc0bf70a8134f452e9a0cc1e89a2ab3ee
Amit Kumar <akamit21@outlook.com>
2019-12-16 17:15:53 +0530

    W9 D1 E

---
+++ submissions/sm_102_amit/week_09/day_1/product/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Product from "./component/Product";
+
+function App() {
+  return (
+    <div className="container-fluid">
+      <h1 className="display-4 text-center">Product Page</h1>
+      <hr />
+      <Product />
+    </div>
+  );
+}
+
+export default App;
