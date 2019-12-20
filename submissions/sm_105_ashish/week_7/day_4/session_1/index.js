0922535eb82049ce8b19b2a1e38bd317c8171206
Ashish379k <au.ashish1@gmail.com>
2019-12-20 20:51:14 +0530

    W7 D5 C2

--- submissions/sm_105_ashish/week_7/day_4/session_1/index.js
+++ submissions/sm_105_ashish/week_7/day_4/session_1/index.js
@@ -1,76 +1,39 @@
 
-// function fib(N){
-//     if(N==2){
-//         return 1
-//     }
-//     else if(N==1){
-//         return 1
-//     }
-//     else if(N==0){
-//         return 0
-//     }
-//     else{
-//         result=fib(N-1)+fib(N-2)
-//         return result
-//     }
-// }
-
-// console.log(fib(3))
+class Car {
+    constructor(carMake = 'Invalid', model =0,topspeed = 0) {
+      this.carMake = make,
+      this.model = model,
+      this.topSpeed = topspeed,
+      this.avgSpeed = Math.floor(this.topSpeed/2) || this.avgSpeed,
+      this.dist+=this.dist
+      this.trips = trips
 
 
-
-var a=[39,27,11,4,24,32,32,1]
-
-// function sumAll(arr,i,result){
-    
-//     if(i<=0){
-//       result.push(-1)
-//         return result
-//     }
-//     if(arr[i]>arr[i+1]){
-//         result.push(arr[i-1])
-//     }
-//     else{
-//         result.push(-1)
-//     }
-//     //arr[i]=arr[i]*arr[i]
-//        return sumAll(arr,i-1,result)
-
-
-// }
-// var b = []
-// sumAll(a,7,b)
-// console.log(b)
-
-
-//var arr=[2,9,6,4]
-
-function mergeSort(org){
-    if (org.length == 1){
-    return org;
     }
-    let mid = Math.floor(org.length/2),
-    left = org.slice(0,mid),
-    right = org.slice(mid);
-    
-    return merge(mergeSort(left),mergeSort(right));
+}
+const lambo = new Car('lamborgini','2015',260)
+  
+class Trip extends Car{
+    constructor(time = 1,dist = 0){
+        super(this.topspeed)
+        this.time = time
+        this.dist = dist
     }
-    
-    function merge(left, right){
-    let l = 0,
-    r = 0,
-    result = [];
-   while(l < left.length && r < right.length){
-    if(left[l] > right[r]){
-    result.push(right[r]);
-    r++;
-    }else{
-    result.push(left[l]);
-    l++;
-    }
-    }
-    return result.concat(left.slice(l),right.slice(r));
-    }
-    
-    var org = [2, 5, 1, 3, 7, 2, 3, 8, 6, 3];
-    console.log(mergeSort(org));
+    avgSpeed(){
+        
+        if(this.time == 1){
+            speed=this.topspeed/2
+            this.time == this.dist/speed
+        }
+     else{
+        speed = this.dist/this.time
+       }
+       }
+}
+console.log(lambo)
+  
+
+
+
+
+
