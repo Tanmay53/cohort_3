b347a2bbd14591773f76388d090bd589a9545971
lalitsheoran <lalit_sheoran@outlook.com>
2019-12-26 18:30:16 +0530

    W8 D1

--- submissions/sm_015_lalit/week_08/session_2/src/components/smallComp.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-// const child = props => <li>{props.toshow}</li>;
-function Child(props){
-    return (
-        <li>{props.toshow}</li>
-    );
-}
-
-export default Child;
