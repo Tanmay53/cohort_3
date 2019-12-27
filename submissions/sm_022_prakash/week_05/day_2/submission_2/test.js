f85e448b0c738fd1a59bce59bc31a564d4dc7332
prakashperam <prakashperam123@gmail.com>
2019-12-27 14:56:38 +0530

    W5 D2

---
+++ submissions/sm_022_prakash/week_05/day_2/submission_2/test.js
@@ -0,0 +1,6 @@
+var arr = [{item: "apple", price: 25, quantity: 20}, {item: "apple", price: 25, quantity: 20},{item: "apple", price: 25, quantity: 20}]
+
+
+for(var i=0; i<arr.length; i++){
+    console.log(arr[i]["price"])
+}
