ec526685de6c908a9775d206ae0c02fd5e2654ae
Krishna Sharma <rohansharma.8574@gmail.com>
2019-12-30 19:53:15 +0530

    W11 D1 E

--- submissions/sm_108_krishna-kant/week_11/day_1/evaluation/src/App.js
+++ submissions/sm_108_krishna-kant/week_11/day_1/evaluation/src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import './App.css';
 import store from './store';
+import { useHistory } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import Navbar from './components/Navigation/Navbar';
 function App() {
+  const history = useHistory();
   return (
     <Provider store={store}>
       <div className='App'>
         <Navbar />
-        <Dashboard />
+        <Dashboard history={history} />
       </div>
     </Provider>
   );
