d4373f8d303e4d22b85c70c8d18a4fb86c5427c4
manishsharma203 <manish1811.sharma@gmail.com>
2019-12-30 22:00:09 +0530

    W8 D1 C1

---
+++ submissions/sm_016_manish-sharma/week_8/day_1/session_1/RJS.1.2/src/components/Settings.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Settings(){
+    return(
+        <div>
+            <h3><span className="badge badge-pill badge-info shadow">SETTINGS</span></h3>
+        </div>
+    )
+}
+export default Settings;
