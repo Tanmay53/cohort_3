2f30ffe1cf770f2f496ddc8926b2d482101bd9a3
Ashish379k <au.ashish1@gmail.com>
2019-12-31 14:21:50 +0530

    W8 D4 C

---
+++ submissions/sm_105_ashish/week_8/day_1/src/components/help.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Help extends React.Component{
+    render(){
+        return (
+            <div>
+               <button class="help">HELP</button>
+            </div>
+        );
+    }
+}
+
+
+export default Help
