8ba0c90ce2858e610865002c0656e1d101f38b82
manishsharma203 <manish1811.sharma@gmail.com>
2019-12-31 23:52:34 +0530

    W8 D1 C2

---
+++ submissions/sm_016_manish-sharma/week_8/day_1/session_2/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Task1 from './components/Task1'
+import Task2 from './components/Task2'
+
+function App() {
+  return (
+    <div>
+      <Task1 />
+      <Task2 />
+    </div>
+  );
+}
+
+export default App;
