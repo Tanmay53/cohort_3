143c274a1c5fa339bce461cd264cb36cc7a2b895
Rahul Sharma <rahulkumarsharma3010@gmail.com>
2020-01-10 18:36:54 +0530

    W10_D1_S2

--- submissions/sm_111_rahul/week_10/day_1/session_2/src/app.js
+++ submissions/sm_111_rahul/week_10/day_1/session_2/src/app.js
@@ -76,7 +76,7 @@ class App extends React.Component {
                         )
                       }
                     >
-                      Mark Incompleted
+                      Mark Completed
                     </button>
                 
                   </li>
