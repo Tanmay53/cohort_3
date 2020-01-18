baba28e34f26989584d8303ba3cefcdc47638698
asheesh900 <thebusinessman999@gmail.com>
2020-01-18 11:39:28 +0530

    W8 D2 C1

---
+++ submissions/sm_104_asheeh/week_8/day_2/session1/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import RestaurantDetails from './Components/RestaurantDetails';
+
+function App() {
+  return (
+    <div className="App">
+      <RestaurantDetails />
+    </div>
+  );
+}
+
+export default App;
