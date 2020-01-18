b4cfad995c5b41107d0198b38fb2ffec757cd518
Rahul Sharma <rahulkumarsharma3010@gmail.com>
2020-01-18 10:20:54 +0530

    W13_D4_S1

---
+++ submissions/sm_111_rahul/week_13/day_4/session_1/count_consonants.py
@@ -0,0 +1,20 @@
+li = ["Aahul", "rohit", "shyam", "kumar"]
+new_li = []
+char_length = 0
+for i in range(0, len(li)):
+    new_li.append(li[i].lower())
+count = 0
+for i in range(0, len(new_li)):
+    for j in range(0, len(new_li[i])):
+        char_length = char_length + 1
+        if new_li[i][j] == "a":
+            count = count + 1
+        if new_li[i][j] == "e":
+            count = count + 1
+        if new_li[i][j] == "i":
+            count = count + 1
+        if new_li[i][j] == "o":
+            count = count + 1
+        if new_li[i][j] == "u":
+            count = count + 1
+print(char_length-count)
