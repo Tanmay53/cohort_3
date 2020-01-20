161d9d24d4d2703f30ca7bdfd37fa3a8571db9b4
venkatasubbu913 <venkatsubbu913@gmail.com>
2020-01-20 16:14:37 +0530

    w4d1s1

---
+++ submissions/sm_034_Venkat/week_4/day_1/session_1/checkSupersetArrays.js
@@ -0,0 +1,30 @@
+function disJointArrays(x,y) {
+    var objX= {}
+    var objY= {}
+    for(i= 0; i< x.length; i++) {
+        objX[x[i]]= i
+    }
+    for(i= 0; i< y.length; i++) {
+        objY[y[i]]= i
+    }
+    var aX= Object.keys(objX)
+    var aY= Object.keys(objY)
+    console.log(aX,aY)
+
+    var c= 0 //c= count
+    for(i= 0; i< aX.length; i++) {
+        for(j= 0; j< aY.length; j++) {
+            if(aX[i]== aY[j]) {
+                c++
+            }
+        }
+    }
+
+    if(c== aY.length) {
+        console.log('true',c)
+    }
+    else {
+        console.log('false',c)
+    }
+}
+disJointArrays([2,5,6,8,4,8,2,9,1],[5,6,4,9,5,6]) //x,y are Two arrys
