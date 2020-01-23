6d75db48f4087662e117c2621cd6c5e79a6102c7
Krishna Sharma <rohansharma.8574@gmail.com>
2020-01-23 15:42:39 +0530

    W14 D3 E

--- submissions/sm_108_krishna-kant/week_14/day_3/evaluation/email_domain.py
+++ submissions/sm_108_krishna-kant/week_14/day_3/evaluation/email_domain.py
@@ -4,11 +4,9 @@ email_address = input("Enter the email address")
 def domain(email) :
     domain_name = ""
     is_domain = False
-    for char in email:
-        if is_domain:
-            domain_name += char
-        if char is "@":
-            is_domain = True
+    for i in range(len(email)):
+        if email[i] is "@":
+            domain_name = email[i+1:]
     return  "Invalid Email Address" if len(domain_name) is 0 else domain_name
 
 print(domain(email_address))
