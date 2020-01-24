5d2355d180de6f59601d6edc0a8eb5187e8dabd7
Krishna Sharma <rohansharma.8574@gmail.com>
2020-01-24 16:47:40 +0530

    W14 D5 C1

--- submissions/sm_108_krishna-kant/week_14/day_5/login.py
+++ submissions/sm_108_krishna-kant/week_14/day_5/login.py
@@ -2,12 +2,17 @@ import csv
 
 def read_csv(username,password):
      usernames = list()
+     res = ""
      with open("/home/coderhex/repos/cohort_3/submissions/sm_108_krishna-kant/week_14/day_5/db/user.csv") as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-           if row['username'] == username and row['password'] == password :
-               return True
-        return False
+            if row['username'] == username and row['password'] == password :
+                res = "Login Succesfull"
+            elif row['username'] == username and row['password'] != password:
+                res = "Wrong Password"
+            else:
+                res = "User Not Found"
+        return res
                
 
 def login():
@@ -16,10 +21,7 @@ def login():
     auth_user = ""
     try:
         auth_user = read_csv(username,password)
-        if auth_user:
-            print("Login Successfull")
-        else:
-            print("User Not Registered")
+        print(auth_user)
     except:
         print("An Error Occurred")
         
