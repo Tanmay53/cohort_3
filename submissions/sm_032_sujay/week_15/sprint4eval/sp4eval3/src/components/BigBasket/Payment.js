5eb852f335fab08c2960b62aac9e01eabc4c26bb
Sujay N <sujayn18@gmail.com>
2020-01-27 18:19:52 +0530

    Sprint4Eval

---
+++ submissions/sm_032_sujay/week_15/sprint4eval/sp4eval3/src/components/BigBasket/Payment.js
@@ -0,0 +1,45 @@
+import React from 'react'
+
+export default class Payment extends React.Component {
+    render() {
+        return (
+            <div>
+                <h4 className="display-4 text-center my-5"> <span className="font-weight-bold">Payment</span> Methods</h4>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-lg-2 offset-2">
+                            <div>
+                                <img src=" http://bbinstant.bigbasket.com/img/Vector%20Smart%20Object-4.png" /></div>
+                            <div>
+
+                                <img src=" http://bbinstant.bigbasket.com/img/Vector%20Smart%20Object-1.png" />
+                            </div>
+
+                            <div>
+
+                                <img src=" http://bbinstant.bigbasket.com/img/Logos.png" />
+                            </div>
+                            <div>
+
+                                <img src=" http://bbinstant.bigbasket.com/img/Vector%20Smart%20Object-5.png" />
+                            </div>
+                            <div>
+
+
+                                <img src=" http://bbinstant.bigbasket.com/img/Vector%20Smart%20Object-2.png" />
+                            </div>
+
+                            <div>
+                                <img src=" http://bbinstant.bigbasket.com/img/Vector%20Smart%20Object-3.png" />
+                            </div>
+
+
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        )
+    }
+}
