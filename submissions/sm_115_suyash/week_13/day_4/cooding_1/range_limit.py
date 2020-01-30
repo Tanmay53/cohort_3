c2bc6cf7b4daccff2d47fef404faa7da8ea79604
SuyashMishra-dev <suyashmishra143143@gmail.com>
2020-01-30 12:38:02 +0530

    W13D4C1

---
+++ submissions/sm_115_suyash/week_13/day_4/cooding_1/range_limit.py
@@ -0,0 +1,4 @@
+for x in range(20):
+    print(x)
+else:
+    print(x+1)
---
+++ submissions/sm_115_suyash/week_13/day_4/cooding_1/reverse_string.py
@@ -0,0 +1,5 @@
+my_str = 'hello'
+rev_str = ''
+for x in range(len(my_str)-1,-1,-1):
+    rev_str += my_str[x]
+print(rev_str)
