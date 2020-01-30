6c625201b24bc7343cf7d75e2c8a4e6180e45d49
Soumik Acharjee <acharjee266@gmail.com>
2020-01-30 10:56:55 +0530

    W13 D4

---
+++ submissions/sm_112_soumik/week_13/day_4/session_1/check_super_set.py
@@ -0,0 +1,5 @@
+string1 = set(input("Enter first set !"))
+string2 = set(input("Enter second set !"))
+
+for letter in string1:
+    if letter in string2
--- submissions/sm_112_soumik/week_13/day_4/sum_even-list.py
+++
@@ -1,5 +0,0 @@
-rng = int(input("Enter a range"))
-to_sum = 0
-for num in range(0, rng, 2):
-    to_sum += num
-print(to_sum)
