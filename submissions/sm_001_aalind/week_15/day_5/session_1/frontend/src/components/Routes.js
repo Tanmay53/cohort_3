1e49d54ff59babdcae3e617f7ad458f1162a96f4
Aalind Sharma <aalinds.sharma@gmail.com>
2020-02-05 16:12:55 +0530

    W15 D5 S1

--- submissions/sm_001_aalind/week_15/day_5/session_1/frontend/src/components/Routes.js
+++ submissions/sm_001_aalind/week_15/day_5/session_1/frontend/src/components/Routes.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import AllUsers from './AllUsers';
+import AddUser from './AddUser';
+import ShowUser from './ShowUser';
+import EditUser from './EditUser';
 
 function Routes() {
   return (
     <React.Fragment>
       <Switch>
-        <Route path='/users/listing' render={() => <AllUsers />} />
+        <Route
+          path='/users/edit/:id'
+          render={props => <EditUser {...props} />}
+        />
+        <Route
+          path='/users/show/:id'
+          render={props => <ShowUser {...props} />}
+        />
+        <Route path='/users/create' render={props => <AddUser {...props} />} />
+        <Route
+          path='/users/listing'
+          render={props => <AllUsers {...props} />}
+        />
         <Route path='/' render={() => <Redirect to='/users/listing' />} />
       </Switch>
     </React.Fragment>
