050ef663f1da3966b45f74ee2c4ba86f6d650543
Deepanshu Prajapati <dipanshuraz2@gmail.com>
2020-02-07 15:21:49 +0530

    W10D1S2

--- submissions/sm_106_deepanshu/week_10/session_2/src/components/Todo.jsx
+++ submissions/sm_106_deepanshu/week_10/session_2/src/components/Todo.jsx
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
 import { Input, Button } from "antd";
 import store from "../redux/store";
-import { addTodo } from "../redux/action";
+import {
+  addTodo,
+  completeTodo,
+  trashTodo,
+  completeToTrash,
+  allClear
+} from "../redux/action";
 
 export class todo extends Component {
   constructor(props) {
@@ -16,7 +22,7 @@ export class todo extends Component {
   };
 
   render() {
-    console.log(store.getState().todoArr);
+    // console.log(store.getState().todoArr);
     return (
       <div className="container my-5 px-3">
         <div className="row">
@@ -29,7 +35,12 @@ export class todo extends Component {
             />
             <Button
               type="primary"
-              onClick={() => store.dispatch(addTodo(this.state.todoItem))}
+              onClick={() => {
+                store.dispatch(addTodo(this.state.todoItem));
+                this.setState({
+                  todoItem: ""
+                });
+              }}
               className="my-2"
               block
             >
@@ -43,14 +54,22 @@ export class todo extends Component {
             <ul style={{ listStyle: "none" }}>
               {store.getState().todoArr.map((elem, i) => {
                 return (
-                  <li className="d-flex justify-content-between">
+                  <li key={i + 1} className="d-flex justify-content-between">
                     <span>
                       <span className="mr-3">{i + 1}</span>
                       <span>{elem}</span>
                     </span>
                     <span>
-                      <span className="px-1">x</span>
-                      <span className="px-1">+</span>
+                      <span className="px-1">
+                        <button onClick={() => store.dispatch(completeTodo(i))}>
+                          +
+                        </button>
+                      </span>
+                      <span className="px-1">
+                        <button onClick={() => store.dispatch(trashTodo(i))}>
+                          x
+                        </button>
+                      </span>
                     </span>
                   </li>
                 );
@@ -59,9 +78,49 @@ export class todo extends Component {
           </div>
           <div className="col-md-6 border my-2">
             <p className="text-dark text-center">Completed</p>
+            <ul>
+              {store.getState().completeTodo.map((elem, i) => {
+                return (
+                  <li key={i + 1}>
+                    <span>
+                      <span>{i + 1}</span>
+                      <span>{elem}</span>
+                    </span>
+                    <span>
+                      <span className="px-1">
+                        <button
+                          onClick={() => store.dispatch(completeToTrash(i))}
+                        >
+                          x
+                        </button>
+                      </span>
+                    </span>
+                  </li>
+                );
+              })}
+            </ul>
           </div>
           <div className="col-md-6 offset-md-3 border my-2">
-            <p className="text-dark text-center">Trash</p>
+            <tr>
+              <td>
+                <p className="text-dark text-center">Trash</p>
+              </td>
+              <td>
+                <button onClick={() => store.dispatch(allClear())}>X</button>
+              </td>
+            </tr>
+            <ul>
+              {store.getState().trashTodo.map((elem, i) => {
+                return (
+                  <li key={i + 1}>
+                    <span>
+                      <span>{i + 1}</span>
+                      <span>{elem}</span>
+                    </span>
+                  </li>
+                );
+              })}
+            </ul>
           </div>
         </div>
       </div>
