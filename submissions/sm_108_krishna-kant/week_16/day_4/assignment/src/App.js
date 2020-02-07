eddebf88519fcf2c18f3f81267b1761769bfae69
Coder-Hex <rohansharma.8574@gmail.com>
2020-02-07 23:30:34 +0530

    W16 D4 A

--- submissions/sm_108_krishna-kant/week_16/day_4/assignment/src/App.js
+++ submissions/sm_108_krishna-kant/week_16/day_4/assignment/src/App.js
@@ -1,9 +1,30 @@
 import React from "react";
-import logo from "./logo.svg";
+import { useLocation, useHistory } from "react-router-dom";
 import "./App.css";
-
+import { PageHeader } from "antd";
+import Routes from "./Routes/Routes";
 function App() {
-  return <div></div>;
+  const location = useLocation();
+  const history = useHistory();
+  return (
+    <div>
+      <PageHeader
+        style={{
+          border: "1px solid rgb(235, 237, 240)"
+        }}
+        onBack={
+          location.pathname === "/"
+            ? null
+            : () => {
+                history.push("/");
+              }
+        }
+        title="Address Book"
+        subTitle="Flask and React based CRUD"
+      />
+      <Routes></Routes>
+    </div>
+  );
 }
 
 export default App;
