81a749d5df9eda8ddf8d9c8a673750057bd06013
Rohit Jaiswal <rht.jaiswl@gmail.com>
2020-02-13 18:21:38 +0530

    W17D4Ev

--- submissions/sm_025_rohit-jaiswal/week_017/day4/form.js
+++ submissions/sm_025_rohit-jaiswal/week_017/day4/form.js
@@ -1,11 +1,16 @@
+
 function setData(){
+    var arr = JSON.parse(localStorage.getItem("arr"))
+    if(!arr){
+      arr = []
+    }
+    console.log(arr)
     var name = document.getElementById("name").value
     var excomp = document.getElementById("excomp").value
     var salary = document.getElementById("salary").value
     var design = document.getElementById("design").value
     var url = document.getElementById("url").value
     var obj = {name: name, excomp: excomp, salary: salary, design: design, url: url};
-    var arr = []
     arr.push(obj)
     localStorage.setItem("arr", JSON.stringify(arr))
   }
