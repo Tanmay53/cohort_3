ca304113d688a0df0bb27b89c571f9ad93ff838a
asheesh900 <thebusinessman999@gmail.com>
2020-02-25 15:48:07 +0530

    Deleted

--- submissions/sm_104_asheeh/week_18/day_3/session1/Backend/server.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from blueprint_auth import auth
-
-app = Flask(__name__)
-app.register_blueprint(auth, url_prefix="/auth")
-
-@app.route("/")
-def home():
-    return "APS page"
