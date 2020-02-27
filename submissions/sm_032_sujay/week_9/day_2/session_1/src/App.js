709957b48cf885b50b3489c3f4d3b3c4be60c7f1
Sujay-N <sujayn18@gmail.com>
2020-02-27 12:32:51 +0530

    W9 D2 C1

---
+++ submissions/sm_032_sujay/week_9/day_2/session_1/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Search from "./components/Axio/Search"
+
+export default class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+      <Search/>
+      
+      </React.Fragment>
+      
+    )
+  }
+}
+
