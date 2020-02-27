7ff1ae8c2619a241042a060fe906de3b07cc4eef
Coder-Hex <rohansharma.8574@gmail.com>
2020-02-27 14:34:33 +0530

    W18 D1

--- submissions/sm_108_krishna-kant/week17/day_6/server/server.py
+++ submissions/sm_108_krishna-kant/week17/day_6/server/server.py
@@ -2,7 +2,7 @@ from flask import Flask
 # Import Blueprints
 from auth import auth
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 app.register_blueprint(auth,url_prefix="/auth")
 
