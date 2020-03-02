e0ca75e6c207f01caec68d7acfac367b27b898a1
Surya Khandavilli <Khandavillisurya@gmail.com>
2020-03-02 19:36:35 +0530

    taskmanager

--- submissions/sm_033_surya/week_19/day_5/task_list/front_end/tasklist/src/components/Tasklists.jsx
+++ submissions/sm_033_surya/week_19/day_5/task_list/front_end/tasklist/src/components/Tasklists.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { CreateTasklist } from './CreateTasklist'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
 
