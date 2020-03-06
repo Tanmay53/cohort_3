436aae480b4e5bef0814317ec7b53093d9143949
asheesh900 <thebusinessman999@gmail.com>
2020-03-06 23:25:54 +0530

    W20 D4 C2

---
+++ submissions/sm_104_asheeh/week_20/day_4/session2/scrape_news.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+
+url_toi = "https://timesofindia.indiatimes.com/briefs"
+url_ndtv = "https://www.ndtv.com/search?searchtext=briefs-"
+response_toi = requests.get(url_toi)
+response_ndtv = requests.get(url_ndtv)
+
+soup_toi = BeautifulSoup(response_toi.text, 'html.parser')
+soup_ndtv = BeautifulSoup(response_ndtv.text, 'html.parser')
+
+briefs_toi = soup_toi('div', {'class': 'brief_box'})
+briefs_ndtv = soup_ndtv('strong', {'class': 'topic_strong_title'})
+
+briefs_list = []
+print("Times Of India Briefs")
+for i in range(30):
+    print(briefs_toi[i].text)
+    briefs_list.append(briefs_toi[i].text)
+    print("--------------")
+    
+print("NDTV Briefs")
+for i in range(25):
+    print(briefs_ndtv[i].text)
+    briefs_list.append(briefs_ndtv[i].text)
+    print("*************")
+
+with open("news.txt", "a") as text_file:
+    for ele in briefs_list:
+        text_file.write(ele)
+        text_file.write("\n")
