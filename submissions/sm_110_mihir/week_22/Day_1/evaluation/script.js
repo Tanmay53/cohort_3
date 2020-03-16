2635bb96a6e9590f5337d71201e074887dc7753c
mihirlaldas <mihirlaldas@gmail.com>
2020-03-16 18:11:34 +0530

    layout and delete

---
+++ submissions/sm_110_mihir/week_22/Day_1/evaluation/script.js
@@ -0,0 +1,6 @@
+function deleteItem() {
+  //   console.log("delete clicked", event.target.parentElement);
+  event.target.parentElement.parentElement.removeChild(
+    event.target.parentElement
+  );
+}
