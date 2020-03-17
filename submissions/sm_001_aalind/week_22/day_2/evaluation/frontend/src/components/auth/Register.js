121ec8311c0ce461cb108b94f32900d2070e6f42
Aalind Sharma <aalinds.sharma@gmail.com>
2020-03-17 20:54:03 +0530

    Final Submission

---
+++ submissions/sm_001_aalind/week_22/day_2/evaluation/frontend/src/components/auth/Register.js
@@ -0,0 +1,114 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      name: '',
+      user_type: ''
+    };
+  }
+
+  formHandler = e => {
+    e.preventDefault();
+    axios.post('http://127.0.0.1:5000/auth/register', this.state).then(res => {
+      alert(res.data.message);
+      if (!res.data.error) {
+        this.props.history.push('/login');
+      }
+    });
+    console.log(this.state);
+  };
+
+  changeHandler = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  render() {
+    return (
+      <div className='container'>
+        <form onSubmit={this.formHandler}>
+          <div className='form-group'>
+            <div className='input-group'>
+              <div className='input-group-prepend'>
+                <span className='input-group-text'>
+                  <i className='fas fa-user' />
+                </span>
+              </div>
+              <input
+                type='text'
+                name='name'
+                onChange={this.changeHandler}
+                className='form-control'
+                placeholder='Name'
+                required
+              />
+            </div>
+          </div>
+          <div className='form-group'>
+            <div className='input-group'>
+              <div className='input-group-prepend'>
+                <span className='input-group-text'>
+                  <i className='fas fa-at' />
+                </span>
+              </div>
+              <input
+                type='email'
+                name='email'
+                // id='email'
+                onChange={this.changeHandler}
+                required
+                className='form-control'
+                placeholder='Email'
+              />
+            </div>
+          </div>
+          <div className='form-group'>
+            <div className='input-group'>
+              <div className='input-group-prepend'>
+                <span className='input-group-text'>
+                  <i className='fas fa-user' />
+                </span>
+              </div>
+              <select
+                name='user_type'
+                onChange={this.changeHandler}
+                required
+                className='form-control'
+              >
+                <option value=''>Select user type</option>
+                <option value='admin'>Admin</option>
+                <option value='user'>User</option>
+              </select>
+            </div>
+          </div>
+          <div className='form-group'>
+            <div className='input-group'>
+              <div className='input-group-prepend'>
+                <span className='input-group-text'>
+                  <i className='fas fa-key' />
+                </span>
+              </div>
+              <input
+                type='password'
+                name='password'
+                onChange={this.changeHandler}
+                required
+                className='form-control'
+                placeholder='Password'
+              />
+            </div>
+          </div>
+          <input type='submit' value='Register' className='btn btn-success' />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Register;
