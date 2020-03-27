0c9eeb4206cb8ec63b0b172660efbfc49ae1fc3f
Coder-Hex <rohansharma.8574@gmail.com>
2020-03-27 15:02:04 +0530

    Backend Routes Complete

---
+++ submissions/sm_108_krishna-kant/week_23/Readme.md
@@ -0,0 +1,13 @@
+# Evaluation 2
+
+## sm_108 Krishna Kant Sharma
+
+### Rubrics
+
+- [x] Able to Add City Stored in list of cities
+- [x] You should be able add a Bus with time and location
+- [x] List all the trips of Buses on Home Page
+- [x] User able to filter all the data based on Source and Destination
+- [x] User should able to select number of rows to be shown in the page
+- [x] User should be able to Update the Bus Details
+- [x] User should be able to Delete Bus Data
--- submissions/sm_108_krishna-kant/week_23/server/server.py
+++ submissions/sm_108_krishna-kant/week_23/server/server.py
@@ -1,8 +1,18 @@
+# imports
 from flask import Flask,jsonify
 from flask_mysqldb import MySQL
+from blueprints.location import location
+from blueprints.buses import buses
 
+
+# Initialize Application
 app = Flask(__name__)
 
+# Registering Blueprint
+app.register_blueprint(location,url_prefix = "/location")
+app.register_blueprint(buses,url_prefix = "/buses")
+
+# Database Configuration
 app.config['MYSQL_HOST'] = "localhost"
 app.config["MYSQL_USER"] = "root"
 app.config["MYSQL_PASSWORD"] = "Krishna@7860"
@@ -11,18 +21,22 @@ app.config["MYSQL_CURSORCLASS"] = "DictCursor"
 
 mysql = MySQL(app)
 
+# @desc : Home Route
+# route: "/"
+# @param : null
+# return : JSON
 @app.route("/")
 def home():
     try:
         conn = mysql.connection.cursor()
         conn.execute("""SELECT DATABASE()""")
         row = conn.fetchone()
-        return jsonify({"row",row}),200
+        return jsonify(row),200
     except Exception as e:
         return jsonify({"error" : str(e)})
     finally:
         conn.close()
-        
+
 
 if __name__ == "__main__" and __package__ is None:
     app.run(debug=True)
