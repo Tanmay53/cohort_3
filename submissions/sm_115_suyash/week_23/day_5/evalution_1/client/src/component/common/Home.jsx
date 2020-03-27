d2b13e98d067c9f0fb4e3add810529800336c9fe
SuyashMishra-dev <suyashmishra143143@gmail.com>
2020-03-27 21:03:51 +0530

    EVL_1

--- submissions/sm_115_suyash/week_23/day_5/evalution_1/client/src/component/common/Home.jsx
+++ submissions/sm_115_suyash/week_23/day_5/evalution_1/client/src/component/common/Home.jsx
@@ -16,31 +16,68 @@ class Home extends Component {
     }
   };
 
+  changePage = e => {
+    if (e.target.value === "prv") {
+      if (this.props.user != undefined) {
+        let obj = {
+          user_id: this.props.user.id,
+          page: this.props.prv
+        };
+        this.props.fetchFollowingTweets(obj);
+      }
+    } else {
+      if (this.props.user != undefined) {
+        let obj = {
+          user_id: this.props.user.id,
+          page: this.props.nxt
+        };
+        this.props.fetchFollowingTweets(obj);
+      }
+    }
+  };
   render() {
     return (
       <div>
-        <h1>All Tweets</h1>
+        <h1 className="text-center">All Tweets</h1>
         <p>
-          <Link to="/profile">Go to your Profile</Link>
+          <Link className="ml-3" to="/profile">
+            Go to your Profile
+          </Link>
         </p>
-        {this.props.followingData != undefined ? (
-          this.props.followingData.map(ele => {
-            return (
-              <div
-                key={ele.id}
-                className="card text-white bg-info mb-3"
-                style={{ maxWidth: "18rem" }}
-              >
-                <div className="card-header">Tweet</div>
-                <div className="card-body">
-                  <h5 className="card-title">{ele.posts}</h5>
+        <div className="d-flex flex-wrap">
+          {this.props.followingData != undefined ? (
+            this.props.followingData.map(ele => {
+              return (
+                <div
+                  key={ele.id}
+                  className="card text-white bg-info mb-3 mx-3 w-100"
+                  style={{ maxWidth: "18rem" }}
+                >
+                  <div className="card-header">Tweet</div>
+                  <div className="card-body">
+                    <h5 className="card-title">{ele.posts}</h5>
+                  </div>
                 </div>
-              </div>
-            );
-          })
-        ) : (
-          <></>
-        )}
+              );
+            })
+          ) : (
+            <></>
+          )}
+        </div>
+        <button
+          className="ml-3 px-3 py-2 text-white btn-dark"
+          value="prv"
+          onClick={this.changePage}
+        >
+          Prev
+        </button>
+        <button
+          className="ml-3 px-3 py-2 text-white btn-dark"
+          value="nxt"
+          onClick={this.changePage}
+        >
+          Next
+        </button>
       </div>
     );
   }
@@ -48,7 +85,9 @@ class Home extends Component {
 
 const mapStateToProps = state => ({
   user: state.user,
-  followingData: state.followingData
+  followingData: state.followingData,
+  prv: state.prv,
+  nxt: state.nxt
 });
 
 const mapDispatchToProps = dispatch => ({
